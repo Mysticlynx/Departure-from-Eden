b920ca2f71396fa6cbf9403033d5c9b60af7ee83
Mysticlynx <chrismorton90@hotmail.com>
2024-01-02 13:57:25 -0800

    GitHub version 1.008
    -Removed <Variants> code left over from old blocks color system.
    -Fixed all <Mountpoints> an <Sides> inconsistencies with Vanilla for Aluminum Armor Blocks.
    -Also Added <PCUConsole> values for Aluminum.
    -Added a Script to remove vanilla BlockCategories.
    -DLC Packs grouped together (Example: Decor 1,2,3 is now just a single BlocksCategory).

---
+++ Data/Scripts/VanillaBlockCategorySupression/VanillaBlockCategorySupression.cs
@@ -0,0 +1,45 @@
+﻿using Sandbox.Definitions;
+using VRage.Game.Components;
+
+namespace VanillaBlockCategorySupression
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class Example_RemoveCategory : MySessionComponentBase
+    {
+        public override void LoadData()
+        {
+            RemoveCategory("LargeBlocks");
+            RemoveCategory("Armorblocks");
+            RemoveCategory("WarfarePack1");
+            RemoveCategory("WarfarePack2");
+            RemoveCategory("DecorativeBlocks1");
+            RemoveCategory("DecorativeBlocks2");
+            RemoveCategory("DecorativeBlocks3");
+            RemoveCategory("GridAutomation");
+            RemoveCategory("FrozenBlocks");
+            RemoveCategory("SparksOfTheFutureBlocks");
+            RemoveCategory("ScrapRacePack");
+            RemoveCategory("IndustrialPack");
+            RemoveCategory("203Pack");
+            // can repeat the above line to remove more
+        }
+
+        void RemoveCategory(string categoryName)
+        {
+            MyGuiBlockCategoryDefinition categoryDef;
+            if(MyDefinitionManager.Static.GetCategories().TryGetValue(categoryName, out categoryDef))
+            {
+                categoryDef.ItemIds.Clear();
+
+                categoryDef.IsBlockCategory = true;
+                categoryDef.IsAnimationCategory = false;
+                categoryDef.IsShipCategory = false;
+                categoryDef.IsToolCategory = false;
+            }
+        }
+
+        protected override void UnloadData()
+        {
+        }
+    }
+}
